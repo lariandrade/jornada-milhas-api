ede0476bc5bcd4b5accac5ff31c78876085b4ec1
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-07-17 13:55:25 -0300

    projeto criado

---
+++ src/test/java/com/challenge/jornadamilhasapi/JornadaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.jornadamilhasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
