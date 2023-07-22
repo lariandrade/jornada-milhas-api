98fbe580638e4202672b5b4f0b3659295fbf0718
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-07-22 14:44:26 -0300

    feat: adicionar paginação

--- src/main/java/com/challenge/jornadamilhasapi/controllers/DepoimentoController.java
+++ src/main/java/com/challenge/jornadamilhasapi/controllers/DepoimentoController.java
@@ -13,6 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -51,12 +54,12 @@ public class DepoimentoController {
             @ApiResponse(responseCode = "404", description = "Nenhum depoimento cadastrado", content = @Content),
     })
     @GetMapping
-    public ResponseEntity listarTodos() {
-        List<Depoimento> depoimentos = depoimentoService.findall();
-        if(depoimentos.isEmpty()) {
+    public ResponseEntity listarTodos(@PageableDefault(sort={"id"})Pageable pageable) {
+        Page<Depoimento> depoimentosPaginados = depoimentoService.findAll(pageable);
+        if (depoimentosPaginados.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum depoimento cadastrado.");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(depoimentos);
+        return ResponseEntity.status(HttpStatus.OK).body(depoimentosPaginados);
     }
 
     @Operation(summary = "Listar um", description = "Será listado o depoimento referente ao ID informado")
